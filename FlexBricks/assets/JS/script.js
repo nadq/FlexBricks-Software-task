ae1a746bc46d14e4ff535aa570452281626d0655
nadq <50573596+nadq@users.noreply.github.com>
2019-06-08 17:07:08 +0300

    Add files via upload

---
+++ FlexBricks/assets/JS/script.js
@@ -0,0 +1,27 @@
+;(function(d,w,$){
+	/* auto width */
+	$('.owl-carousel').owlCarousel({
+    margin:3,
+    loop:true,
+    autoWidth:true,
+    items:4
+	})
+	/* timer plugin */
+	// var owl = $('.owl-carousel');
+	// owl.owlCarousel({
+	//     items:4,
+	//     loop:true,
+	//     margin:10,
+	//     autoplay:true,
+	//     autoplayTimeout:1000,
+	//     autoplayHoverPause:true
+	// });
+	// $('.play').on('click',function(){
+	//     owl.trigger('play.owl.autoplay',[1000])
+	// })
+	// $('.stop').on('click',function(){
+	//     owl.trigger('stop.owl.autoplay')
+	// })
+
+})(document,window,jQuery);
+
